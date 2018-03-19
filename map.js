1f296e3b6970fc10c9bb921dbbcd233a55be95ad
Sajjad Anwar <sajjadkm@gmail.com>
2018-03-19 15:23:10 +0530

    geojson geometry type filter

--- map.js
+++ map.js
@@ -5,43 +5,53 @@ var ff = require('feature-filter');
 var featureCollection = require('turf-featurecollection');
 
 /**
- * Filters the OSM features in a single tile based on given filter.
- * @module
- * @type {number}
- */
+* Filters the OSM features in a single tile based on given filter.
+* @module
+* @type {number}
+*/
 module.exports = function (data, tile, writeData, done) {
-
+    var types = {
+        Point: 1,
+        MultiPoint: 1,
+        LineString: 2,
+        MultiLineString: 2,
+        Polygon: 3,
+        MultiPolygon: 3
+    };
     var filter = (mapOptions.tagFilter) ? ff(mapOptions.tagFilter) : false;
     var layer = data.osm.osm;
     var osmID = (mapOptions.count) ? [] : null;
     var dates = Boolean(mapOptions.dates) ? parseDates(mapOptions.dates) : false;
     var users = mapOptions.users;
     var result = layer.features.filter(function (val) {
+        var valOperator = {
+            'properties': val.properties,
+            'type': types[val.geometry.type]
+        };
+        if ((!users || (users && users.indexOf(valOperator.properties['@user']) > -1)) && (
+            !mapOptions.dates || (mapOptions.dates && valOperator.properties['@timestamp'] && valOperator.properties['@timestamp'] >= dates[0] && valOperator.properties['@timestamp'] <= dates[1])) && (!filter || (filter && filter(valOperator)))) {
 
-        if ((!users || (users && users.indexOf(val.properties['@user']) > -1)) && (
-            !mapOptions.dates || (mapOptions.dates && val.properties['@timestamp'] && val.properties['@timestamp'] >= dates[0] && val.properties['@timestamp'] <= dates[1])) && (!filter || (filter && filter(val)))) {
+                if (mapOptions.count) {
+                    osmID.push(valOperator.properties['@id']);
+                }
 
-            if (mapOptions.count) {
-                osmID.push(val.properties['@id']);
+                return true;
             }
+        });
 
-            return true;
+        if (mapOptions.tmpGeojson && result.length > 0) {
+            var fc = featureCollection(result);
+            fs.appendFileSync(mapOptions.tmpGeojson, JSON.stringify(fc) + '\n');
         }
-    });
-
-    if (mapOptions.tmpGeojson && result.length > 0) {
-        var fc = featureCollection(result);
-        fs.appendFileSync(mapOptions.tmpGeojson, JSON.stringify(fc) + '\n');
-    }
-    done(null, osmID);
-};
+        done(null, osmID);
+    };
 
 /**
- @function parseDates
- @description Convert Date to timestamp. If endDate is not present, it is set as next immediate date to startDate.
- @param {string[]} strings
- @return {number[]}
- */
+@function parseDates
+@description Convert Date to timestamp. If endDate is not present, it is set as next immediate date to startDate.
+@param {string[]} strings
+@return {number[]}
+*/
 function parseDates(dates) {
     var startDate = new Date(dates[0]);
     var endDate = new Date(dates[dates.length - 1]);
